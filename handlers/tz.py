d279031da759503cdde8c34102bda8c9fc7ad438
avemooneez <mooneez.mnz@gmail.com>
2024-07-12 18:09:50 +0300

    Пофикшен баг с застреванием FSM на первом этапе

--- handlers/tz.py
+++ handlers/tz.py
@@ -4,10 +4,11 @@ from aiogram import Router, F
 from aiogram.filters import StateFilter
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
-from keyboards import geolocation
+from keyboards import geolocation, main
 from utils import geo
 import datetime
 import pytz
+import asyncio
 from db import Database
 
 
@@ -17,19 +18,23 @@ db = Database("./database.db")
 class GetTimeZone(StatesGroup):
     location = State()
 
-@router.message(Command("timezone"), StateFilter(None))
+@router.message(StateFilter(None), Command("timezone"))
 async def cmd_geo(message: Message, state: FSMContext):
     await message.answer(
         "🌍 Для корректной работы боту необходимо знать ваш часовой пояс. Пожалуйста, отправьте вашу геолокацию по кнопке ниже.",
         reply_markup=geolocation.geolocation()
         )
-    state.set_state(GetTimeZone.location)
+    await state.set_state(GetTimeZone.location)
 
-@router.message(F.location, GetTimeZone.location)
-async def on_location(message: Message):
+@router.message(GetTimeZone.location)
+async def on_location(message: Message, state: FSMContext):
     timezone = geo.tz(lon=message.location.longitude, lat=message.location.latitude)
     time = datetime.datetime.now(tz=pytz.timezone(timezone)).strftime("%H:%M")
     await message.answer(f"🌍 Ваш часовой пояс: {timezone}\n"
-                         f"🕐 Ваше время: {time}", reply_markup=ReplyKeyboardRemove()
+                         f"🕐 Ваше время: {time}", reply_markup=main.main()
                          )
     db.add_tz(timezone, message.from_user.id)
+    await asyncio.sleep(1.5)
+    await message.answer("Отправьте Вашу геолокацию по кнопке ниже, напишите город или введите координаты, и я пришлю погоду в данном участке.",
+                         reply_markup=main.main())
+    await state.set_state(None)
--- keyboards/geolocation.py
+++ keyboards/geolocation.py
@@ -3,6 +3,6 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 def geolocation() -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardBuilder()
-    kb.button(text="Отправить свою геолокацию", request_location=True)
+    kb.button(text="Отправить геолокацию", request_location=True)
     kb.adjust(2)
     return kb.as_markup(resize_keyboard=True)
