34f3c469fc7d1e0fa28f7e0699fdcccd395d8758
avemooneez <mooneez.mnz@gmail.com>
2024-11-01 22:55:44 +0300

    Добавлен русский язык

--- utils/translator.py
+++ utils/translator.py
@@ -7,8 +7,8 @@ translations = {
                 'answer' : 'Location: {loc} \n\n',
             },
             'start' : {
-                'welcome_new_user' : 'Hello! You are a new user. This is a telegram bot that can send the weather.\nEnter the time when it is better to send the weather every day or click the button below so that I do not send you the weather every day. You can always change your mind in /settings.',
-                'welcome' : 'Welcome!\nSend your geolocation using the button below and I will send you the weather in this area.',
+                'welcome_new_user' : 'Hello! You are a new user. This is a telegram bot that can send the weather.\nEnter the time when it is better to send the weather every day or click the button below so that I do not send you the weather every day. You can always change your mind in /settings',
+                'welcome' : 'Welcome!\nSend your geolocation using the button below and I will send you the weather in this area',
                 'answer_is_given' : 'OK!',
                 'incorrect_time' : 'Please enter a valid time!'
             },
@@ -42,10 +42,31 @@ translations = {
     },
     'ru' : {
         'handlers' : {
-            
+            'forecast' : {
+                'get_location' : 'Пожалуйста, отправьте ваше местоположение по кнопке ниже',
+                'forecast_msg' : '{date}\n{weather_emoji}{wthr}\n🌞Температура: {ceiled_temp} °C\n💨Ветер: {wind} м/с | {degree}\n🌡Давление: {ceiled_pressure} мм рт. ст.\n\n',
+                'answer' : 'Локация: {loc} \n\n',
+            },
+            'start' : {
+                'welcome_new_user' : 'Привет! Ты новый пользователь. Это — телеграм-бот, который может отправлять погоду.\nВведи время, в которое лучше отправлять погоду каждый день или нажми кнопку ниже, чтобы я не отправлял тебе погоду каждый день. Ты всегда можешь изменить своё решение в /settings',
+                'welcome' : 'Добро пожаловать!\nОтправьте Вашу геолокацию по кнопке ниже и я пришлю погоду в данном участке',
+                'answer_is_given' : 'Принято!',
+                'incorrect_time' : 'Введите корректное время!'
+            },
+            'weather' : {
+                'answer' : 'Локация: {loc} | {date}\n\n{weather_emoji}{wthr}\n🌞Температура: {ceiled_temp} °C\n💨Ветер: {wind} м/с | {degree}\n🌡Давление: {ceiled_pressure} мм рт. ст.'
+            }
         },
         'keyboards' : {
-            
+            'geo' : {
+                'loc' : 'Отправить свою геолокацию'
+            },
+            'main' : {
+                'loc' : 'Отправить свою геолокацию'
+            },
+            'timechooser' : {
+                'no_time' : 'Нет, спасибо'
+            }
         },
         'utils' : {
             'weather' : {
