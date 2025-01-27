ee5c54e6c552d69b6abed57d4f67bf5e13c2f2a7
avemooneez <mooneez.mnz@gmail.com>
2025-01-27 10:21:26 +0300

    Вырезана функция отправки погоды по времени

--- handlers/settings.py
+++ handlers/settings.py
@@ -45,81 +45,81 @@ async def cb_lang_en(callback: CallbackQuery):
 
 # Время ежедневной отправки погоды
 
-@router.callback_query(F.data == "setting_time", StateFilter(None))
-async def cb_change_time(callback: CallbackQuery, state: FSMContext):
-    lang = db.get_lang(callback.from_user.id)
-    time = db.get_time(user_id=callback.from_user.id)
-    if time is None:
-        time = translator.get_translation(lang=lang, firstKey='handlers', secondKey='settings', thirdKey='time_is_not_set')
-    answer = translator.get_translation(
-        lang=lang,
-        firstKey='handlers',
-        secondKey='settings',
-        thirdKey='choose_time',
-        time=time
-    )
-    await callback.message.edit_text(answer, reply_markup=settings.choose_time(lang))
-    await state.set_state(TimeWeather.time)
+# @router.callback_query(F.data == "setting_time", StateFilter(None))
+# async def cb_change_time(callback: CallbackQuery, state: FSMContext):
+#     lang = db.get_lang(callback.from_user.id)
+#     time = db.get_time(user_id=callback.from_user.id)
+#     if time is None:
+#         time = translator.get_translation(lang=lang, firstKey='handlers', secondKey='settings', thirdKey='time_is_not_set')
+#     answer = translator.get_translation(
+#         lang=lang,
+#         firstKey='handlers',
+#         secondKey='settings',
+#         thirdKey='choose_time',
+#         time=time
+#     )
+#     await callback.message.edit_text(answer, reply_markup=settings.choose_time(lang))
+#     await state.set_state(TimeWeather.time)
 
-@router.callback_query(TimeWeather.time)
-async def cb_time(callback: CallbackQuery, state: FSMContext):
-    await callback.answer()
-    lang = db.get_lang(callback.from_user.id)
-    time_str = callback.data
+# @router.callback_query(TimeWeather.time)
+# async def cb_time(callback: CallbackQuery, state: FSMContext):
+#     await callback.answer()
+#     lang = db.get_lang(callback.from_user.id)
+#     time_str = callback.data
 
-    if time_str == "setting_time_no_time":
-        answer = translator.get_translation(
-            lang=lang,
-            firstKey='handlers',
-            secondKey='settings',
-            thirdKey='no_time'
-        )
-        db.set_time(time=None, user_id=callback.from_user.id)
-        time = db.get_time(user_id=callback.from_user.id)
-        if time is None:
-            time = translator.get_translation(lang=lang, firstKey='handlers', secondKey='settings', thirdKey='time_is_not_set')
-        msg = translator.get_translation(
-            lang=lang,
-            firstKey='handlers',
-            secondKey='settings',
-            thirdKey='choose_time',
-            time=time
-        )
-        await callback.message.edit_text(msg, reply_markup=settings.choose_time(lang))
-        await callback.answer(answer, reply_markup=main.main(lang=lang))
-        await state.clear()
-        return
+#     if time_str == "setting_time_no_time":
+#         answer = translator.get_translation(
+#             lang=lang,
+#             firstKey='handlers',
+#             secondKey='settings',
+#             thirdKey='no_time'
+#         )
+#         db.set_time(time=None, user_id=callback.from_user.id)
+#         time = db.get_time(user_id=callback.from_user.id)
+#         if time is None:
+#             time = translator.get_translation(lang=lang, firstKey='handlers', secondKey='settings', thirdKey='time_is_not_set')
+#         msg = translator.get_translation(
+#             lang=lang,
+#             firstKey='handlers',
+#             secondKey='settings',
+#             thirdKey='choose_time',
+#             time=time
+#         )
+#         await callback.message.edit_text(msg, reply_markup=settings.choose_time(lang))
+#         await callback.answer(answer, reply_markup=main.main(lang=lang))
+#         await state.clear()
+#         return
 
-    try:
-        time = datetime.strptime(time_str, '%H:%M')
+#     try:
+#         time = datetime.strptime(time_str, '%H:%M')
 
-        if time.hour < 0 or time.hour > 23 or time.minute < 0 or time.minute > 59:
-            raise ValueError("Time out of range")
+#         if time.hour < 0 or time.hour > 23 or time.minute < 0 or time.minute > 59:
+#             raise ValueError("Time out of range")
 
-        db.set_time(time=time_str, user_id=callback.from_user.id)
+#         db.set_time(time=time_str, user_id=callback.from_user.id)
         
-        answer = translator.get_translation(
-            lang=lang,
-            firstKey='handlers',
-            secondKey='settings',
-            thirdKey='choose_time',
-            time=time
-        )
-        await callback.message.edit_reply_markup(answer, reply_markup=main.main(lang=lang))
-        answer = translator.get_translation(
-            lang=lang,
-            firstKey='handlers',
-            secondKey='settings',
-            thirdKey='time_set'
-        )
-        await callback.answer(answer, reply_markup=main.main(lang=lang))
-        await state.clear()
-    except Exception:
-        answer = translator.get_translation(
-            lang=lang,
-            firstKey='handlers',
-            secondKey='settings',
-            thirdKey='invalid_time'
-        )
-        await callback.answer(answer, reply_markup=settings.choose_time(lang))
-        await state.set_state(TimeWeather.time)
+#         answer = translator.get_translation(
+#             lang=lang,
+#             firstKey='handlers',
+#             secondKey='settings',
+#             thirdKey='choose_time',
+#             time=time
+#         )
+#         await callback.message.edit_reply_markup(answer, reply_markup=main.main(lang=lang))
+#         answer = translator.get_translation(
+#             lang=lang,
+#             firstKey='handlers',
+#             secondKey='settings',
+#             thirdKey='time_set'
+#         )
+#         await callback.answer(answer, reply_markup=main.main(lang=lang))
+#         await state.clear()
+#     except Exception:
+#         answer = translator.get_translation(
+#             lang=lang,
+#             firstKey='handlers',
+#             secondKey='settings',
+#             thirdKey='invalid_time'
+#         )
+#         await callback.answer(answer, reply_markup=settings.choose_time(lang))
+#         await state.set_state(TimeWeather.time)
--- keyboards/settings.py
+++ keyboards/settings.py
@@ -5,21 +5,24 @@ from utils import translator
 def choose_setting(lang) -> InlineKeyboardMarkup:
     kb = InlineKeyboardBuilder()
     
-    for key in ['time', 'language']:
-        kb.button(text=translator.get_translation(
-            lang=lang, firstKey='keyboards', secondKey='settings',
-            thirdKey='choose_setting_' + key), callback_data='setting_' + key)
-    kb.adjust(1)
-    return kb.as_markup(resize_keyboard=True)
-
-def choose_time(lang) -> InlineKeyboardMarkup:
-    kb = InlineKeyboardBuilder()
-    
+    # for key in ['time', 'language']:
+    #     kb.button(text=translator.get_translation(
+    #         lang=lang, firstKey='keyboards', secondKey='settings',
+    #         thirdKey='choose_setting_' + key), callback_data='setting_' + key)
     kb.button(text=translator.get_translation(
         lang=lang, firstKey='keyboards', secondKey='timechooser',
         thirdKey='no_time'), callback_data='setting_time_no_time')
     kb.adjust(1)
     return kb.as_markup(resize_keyboard=True)
+
+# def choose_time(lang) -> InlineKeyboardMarkup:
+#     kb = InlineKeyboardBuilder()
+    
+#     kb.button(text=translator.get_translation(
+#         lang=lang, firstKey='keyboards', secondKey='timechooser',
+#         thirdKey='no_time'), callback_data='setting_time_no_time')
+#     kb.adjust(1)
+#     return kb.as_markup(resize_keyboard=True)
 
 def choose_language(lang) -> InlineKeyboardMarkup:
     kb = InlineKeyboardBuilder()
